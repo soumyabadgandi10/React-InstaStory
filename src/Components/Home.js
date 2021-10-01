ea4f6057a056fd30442c96f13636b661bc8832ec
soumyabadgandi10 <soumyabadgandi10@users.noreply.github.com>
2021-10-01 21:28:19 +0530

    added category

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import "./Home.css";
+import categoryItems  from "../dataSource/categories";
 
 export default function Home() {
-    return (
+    function renderCategoryItem() {
+        
+        return categoryItems.map((categoryItem)=>{
+            return (
+                <div className="category-text" 
+                key={categoryItem.name}>
+                <div className="category-outer-circle">
+                    <div className="category-inner-circle ">
+                        <i className="material-icons">{categoryItem.iconName}</i>
+                    </div>
+                </div>
+                <div>{categoryItem.name}</div>
+                </div>
+            );
+        });
+    }
+      return (
         <div>
             <div className="header">Stories Feed</div>
+            <div className="grid-container">{renderCategoryItem()}</div>
         </div>
-    )
+    );
 }
+
