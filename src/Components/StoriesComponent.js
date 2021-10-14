bb04571c36852886251a3f4480e59f2609353e1e
Satyam-code143 <Satyam-code143@github.com>
2021-10-14 20:57:18 +0530

    satyams changes

--- src/Components/StoriesComponent.js
+++ src/Components/StoriesComponent.js
@@ -1,103 +1,66 @@
-import React from 'react';
-import { useRouteMatch, useHistory } from 'react-router-dom';
+import React from "react";
+import { useRouteMatch, useHistory } from "react-router-dom";
 //import { useParams } from 'react-router-dom';
-import Stories from 'react-insta-stories';
-import StoriesData from '../dataSource/StoriesData';
+import Stories from "react-insta-stories";
+import StoriesData from "../dataSource/StoriesData";
 
 export default function StoriesComponent() {
-    const match = useRouteMatch();
-    const history = useHistory();
-    function goBackToHomePage() {
-        history.push("/");
+  const match = useRouteMatch();
+  const history = useHistory();
+  function goBackToHomePage() {
+    history.push("/");
+  }
+  const categoryName = match.params.categoryName;
+  const stories = StoriesData[categoryName].map(
+    (
+      { backgroundImageUrl, title, captionColor, caption, backgroundColor },
+      index
+    ) => {
+      return {
+        content: ({ action, isPaused }) => {
+          return (
+            <div
+              style={{
+                width: "100%",
+                backgroundColor:
+                  backgroundColor === null ? null : backgroundColor,
+                height: "100vh",
+              }}
+            >
+              <h1
+                style={{
+                  fontSize: 22,
+                  color: captionColor === null ? null : "white",
+                }}
+              >
+                {title}
+              </h1>
+              <img src={backgroundImageUrl} width="50%" height="80%" />
+              <p
+                style={{
+                  color: "white",
+                  fontSize: 14,
+                  textAlign: "center",
+                  width: "50%",
+                  alignSelf: "center",
+                }}
+              >
+                {caption}
+              </p>
+            </div>
+          );
+        },
+      };
     }
-        const categoryName = match.params.categoryName;
-        const stories = StoriesData[categoryName].map((storyItem) =>{
-            if (storyItem.type ==="imageCaptionPost"){
-                return {
-                    content: ({ action, isPaused }) => {
-                        return (
-                                <div style={
-                                    {
-                                    backgroundPosition: "center",
-                                    backgroundRepeat: "no-repeat",
-                                    width: "100vw",
-                                    height: "100vh",
-                                    backgroundImage: `url(${storyItem.backgroundImageUrl})`,
-
-                                    }}>
-                                  <div style={
-                                      {
-                                          fontSize: "2rem",
-                                          lineHeight: "2rem",
-                                          color: "white",
-                                          marginTop: "5px",
-                                      }}>
-                                     <span>{storyItem.caption}</span> 
-                                  </div>  
-                                </div>
-                            );
-                    },
-                }
-            }
-            else if(storyItem.type === "textImagePost"){
-                return {
-                    content: ({ action, isPaused }) => {
-                        return (
-                                <div style={
-                                    {
-                                    backgroundPosition: "center",
-                                    width: "100vw",
-                                    height: "100vh",
-                                    backgroundColor: storyItem.backgroundColor,
-                                    }}>
-                                  
-                                  <div style={
-                                      {
-                                          fontSize: "2rem",
-                                          lineHeight: "2rem",
-                                          color: "white",
-                                          marginTop: "5px",
-                                      }}>
-                                     <span>{storyItem.title}</span> 
-                                  </div>
-                                  <div style={
-                                      {
-                                          width: "100vw",
-                                          
-                                        }} >
-                                      <img src={storyItem.postImageUrl} 
-                                          style={
-                                              {
-                                                height: "50vh",
-                                              }
-                                          }
-                                      />
-                                    </div> 
-                                  <div style={
-                                      {
-                                        
-                                          textAlign: "center",
-                                          color: "white",
-                                          fontSize: "1.2rem",
-                                      }
-                                  }>
-                                     <span>{storyItem.text} </span> 
-                                  </div>
-                                </div>
-                            );
-                    },
-                }
-            }
-    } );
-    return (
-        <div>
-            <Stories  
-            stories={stories} 
-                width={"100vw"}
-                height={"100vh"}
-                onAllStoriesEnd={() => goBackToHomePage()}
-                
-            />
-        </div>
-    );
-};
+  );
+  return (
+    <div>
+      <Stories
+        stories={stories}
+        width={"100vw"}
+        height={"100vh"}
+        onAllStoriesEnd={() => goBackToHomePage()}
+      />
+    </div>
+  );
+}
